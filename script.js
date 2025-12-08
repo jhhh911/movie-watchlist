878f1a053344478219cb1cd8a3f64fd1a08b6280
James Heiniger <james.heiniger911@gmail.com>
2025-12-08 17:58:14 +1030

    Added js for movie content, search generates html on the page but style will need fixing.

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-const search = document.getElementById('search')
-const searchBtn = document.getElementById('search-btn')
+const search = document.getElementById("search");
+const searchBtn = document.getElementById("search-btn");
+let movieArray = [];
+let idArray = [];
 
-  function searchMovie() {
-    fetch(`http://www.omdbapi.com/?apikey=f7bb0b29&s=${search.value}`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  }
+function fetchData() {
+  document.getElementById("placeholder").classList.add("none");
+  movieArray = [];
+  idArray = [];
+  fetch(`https://www.omdbapi.com/?apikey=f7bb0b29&s=${search.value}`)
+    .then(res => res.json())
+    .then(data => {
+      movieArray = data.Search;
+      movieArray.forEach(movie => {
+        idArray.push(movie.imdbID);
+      });
+      renderSearchedMovies();
+    });
+}
+function renderSearchedMovies() {
+  let html = "";
+  idArray.forEach(id => {
+    fetch(`https://www.omdbapi.com/?apikey=f7bb0b29&i=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const movieRating = data.Ratings
+        html += `
+        <div class='item-container'>
+<img src=${data.Poster} alt="${data.Title} movie poster" class="movie-img" />
+        <div class="movie-text">
+          <div class="movie-info-1">
+            <h3 class="movie-title">${data.Title}</h3>
+            <div class="movie-rating">⭐ ${movieRating.Value}</div>
+          </div>
 
-  searchBtn.addEventListener('click', searchMovie)
+          <div class="movie-info-2">
+            <div class="movie-time">${data.Runtime}</div>
+            <div class="movie-genres">${data.Genre}</div>
+            <div class="add-to-watchlist">
+              <img
+                src="./img/Icon (1).png"
+                alt="Add to watchlist button"
+                class="add-icon"
+              />
+              <p>Watchlist</p>
+            </div>
+          </div>
+          <div class="movie-info-3">
+            <p>${data.Plot}</p>
+          </div>
+        </div>
+        <div class="border-bottom"></div>
+        </div>
+`;
+        document.getElementById("movie-input").innerHTML = html;
+        console.log(data.Ratings)
+      });
+  });
+}
+
+searchBtn.addEventListener("click", fetchData);
